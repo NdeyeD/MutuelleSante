732b77fc8e7194baef6fd1c74ae280e59a713520
Cheikh Ahmadou Bamba Kamara <cheikha.b.kamara@mbp-de-cabk.home>
2023-06-11 10:13:07 +0000

    Initial commit

---
+++ src/main/java/com/example/mutuellesante/MutuelleSanteApplication.java
@@ -0,0 +1,13 @@
+package com.example.mutuellesante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutuelleSanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutuelleSanteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mutuellesante/MutuelleSanteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mutuellesante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutuelleSanteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
